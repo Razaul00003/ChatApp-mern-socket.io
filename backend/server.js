2e3d6179a0c987698e6d444496312f93b62d540b
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-17 22:37:33 +0600

    added state management and backend with mongodb mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000;
 
+const databaseConnect = require("./config/database");
 app.get("/", (req, res) => {
   res.send("welcome to backend homepage");
+});
+
+databaseConnect();
+dotenv.config({
+  path: "backend/config/config.env",
 });
 app.listen(PORT, console.log(`server is running on ${PORT}`));
