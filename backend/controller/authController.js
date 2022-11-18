3c089038664144b472fbdf758d6622012b8dba97
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-18 15:23:00 +0600

    added login functionality

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -4,7 +4,7 @@ const registerModel = require("../models/authModel");
 const fs = require("fs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { options } = require("../routes/authRoute");
+const console = require("console");
 
 module.exports.userRegister = (req, res) => {
   const form = formidable();
@@ -93,7 +93,6 @@ module.exports.userRegister = (req, res) => {
                   Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000
                 ),
               };
-              console.log("registered", token);
 
               res.status(201).cookie("authToken", token, options).json({
                 successMessage: "Your Register Successful",
@@ -117,4 +116,84 @@ module.exports.userRegister = (req, res) => {
       }
     }
   }); // end Formidable
+};
+
+module.exports.userLogin = async (req, res) => {
+  const error = [];
+  const { email, password } = req.body;
+  if (!email) {
+    error.push("Please provide your Email");
+  }
+  if (!password) {
+    error.push("Please provide your Passowrd");
+  }
+  if (email && !validator.isEmail(email)) {
+    error.push("Please provide your Valid Email");
+  }
+  if (error.length > 0) {
+    res.status(400).json({
+      error: {
+        errorMessage: error,
+      },
+    });
+  } else {
+    try {
+      const checkUser = await registerModel
+        .findOne({
+          email: email,
+        })
+        .select("+password");
+
+      if (checkUser) {
+        const matchPassword = await bcrypt.compare(
+          password,
+          checkUser.password
+        );
+
+        if (matchPassword) {
+          const token = jwt.sign(
+            {
+              id: checkUser._id,
+              email: checkUser.email,
+              userName: checkUser.userName,
+              image: checkUser.image,
+              registerTime: checkUser.createdAt,
+            },
+            process.env.SECRET,
+            {
+              expiresIn: process.env.TOKEN_EXP,
+            }
+          );
+          const options = {
+            expires: new Date(
+              Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000
+            ),
+          };
+
+          res.status(200).cookie("authToken", token, options).json({
+            successMessage: "Your Login Successful",
+            token,
+          });
+        } else {
+          res.status(400).json({
+            error: {
+              errorMessage: ["Your Password not Valid"],
+            },
+          });
+        }
+      } else {
+        res.status(400).json({
+          error: {
+            errorMessage: ["Your Email Not Found"],
+          },
+        });
+      }
+    } catch {
+      res.status(404).json({
+        error: {
+          errorMessage: ["Internal Sever Error"],
+        },
+      });
+    }
+  }
 };
