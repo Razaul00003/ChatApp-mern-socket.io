6a3b5451168388c9df1c0f592e7bc06fc74cd0a9
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-21 11:03:56 +0600

    protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Messenger from "./components/Messenger";
+import ProtectRoute from "./components/ProtectRoute";
 import Register from "./components/Register";
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/messenger/login" element={<Login />} />
-        <Route path="/messenger/register" element={<Register />} />
-        <Route path="/" element={<Messenger />} />
-      </Routes>
-    </Router>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/messenger/login" element={<Login />} />
+          <Route path="/messenger/register" element={<Register />} />
+
+          <Route
+            path="/"
+            element={
+              <ProtectRoute>
+                {" "}
+                <Messenger />{" "}
+              </ProtectRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      ,
+    </div>
   );
 }
+
+export default App;
